7a8eb89c36ad7681a94b813dd70a22a93551186b
Kimhyunmook <gusanr4200@naver.com>
2024-09-27 23:27:01 +0900

    임시저장

--- app/component/cardUi.tsx
+++ app/component/cardUi.tsx
@@ -1,11 +1,19 @@
 "use client"
+import card from './card.module.css'
 interface Card {
     children:React.ReactElement|any
 }
 export default function Card () {
     return (
-        <div className="card">
-            
+        <div className={`${card.card}`}>
+            <div className={`${card.textBox}`}>
+                <h2>
+                    제목입니다.
+                </h2>
+                <p>
+                    글입니다.
+                </p>
+            </div>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
 'use client'
 import styles from "./page.module.css";
 import LayoutControl from "./component/layoutControl";
+import Card from "./component/cardUi";
+import cardStyle from './component/card.module.css'
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <LayoutControl noDisplay={[]}>
+        <main className={`${styles.main} flex_center`}>
+          <div className={`${cardStyle.cardBox} content`}>
+            <h3 className={`${styles.cardLength}`}>
+                총 200개
+            </h3>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+          </div>
+        </main>
       </LayoutControl>
     </div>
   );
